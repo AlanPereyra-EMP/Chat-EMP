4ac860016b2204fc9ff18dac61ccebbfbf0d152e
Alan Pereyra <alanpereyra33@gmail.com>
2021-12-30 17:40:28 -0300

    add first try to use service worker

--- includes/js/cemp-notifications.js
+++ includes/js/cemp-notifications.js
@@ -1,38 +1,41 @@
 function cempNotificationPermission(){
-  if(!Notification){
-    console.log("Este navegador no soporta notificaciones.");
+  if(!('Notification' in window)){
+    alert("Este navegador no soporta las notificaciones");
     return;
-  }else{
-    if(checkNotificationPromise()){
-      Notification.requestPermission()
-      .then((permission) => {
-        handlePermission(permission);
-      })
-    }else{
-      Notification.requestPermission(function(permission){
-        handlePermission(permission);
-      });
-    }
-  }
-  function handlePermission(permission){
-    console.log(permission);
+  }else if(Notification.permission === "granted"){
+    Notification.requestPermission(function(permission){
+      console.log(permission);
+    });
+  }else if(Notification.permission !== 'denied'){
+    Notification.requestPermission()
+    .then((permission) => {
+      console.log(permission);
+    })
   }
 }
-function checkNotificationPromise(){
-  try{
-    Notification.requestPermission().then();
-  }catch(e){
-    return false;
-  }
-  return true;
+cempNotificationPermission();
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
 }
-cempNotificationPermission()
 
 var notiSound = new Audio(cempAudio.notification);
 function cempNotification(msg, body){
-  new Notification(msg,
-    {icon: cempFav,
-     body: body
-    });
+  var options = {
+    icon: cempFav,
+    body: body
+  }
+  var n = new Notification(msg, options);
+  setTimeout(n.close.bind(n), 5000);
   notiSound.play();
+
+  alert('asd');
 }
